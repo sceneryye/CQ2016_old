4df9009e8f536a580d43194ad8bcaba1f5f34507
Scenery Ye <scenery.ye@gmail.com>
2016-04-06 10:09:29 +0800

    add auto clear log

---
+++ config/initializers/clear_logs.rb
@@ -0,0 +1,13 @@
+# config/initializers/clear_logs.rb
+if Rails.env.development?
+  MAX_LOG_SIZE = 2.megabytes
+
+  logs = [ File.join(Rails.root, 'log', 'development.log'), File.join(Rails.root, 'log', 'test.log') ]
+
+  logs.each do |log|
+    if File.size?(log).to_i > MAX_LOG_SIZE
+      $stdout.puts "Removing Log: #{log}"
+      `rm #{log}`
+    end
+  end
+end
