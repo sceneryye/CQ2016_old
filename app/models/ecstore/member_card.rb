a334cd9be117099cd81fb762dabd5cc34e99a47b
Scenery Ye <scenery.ye@gmail.com>
2016-04-13 06:35:40 +0800

    member cards

--- app/models/ecstore/member_card.rb
+++ app/models/ecstore/member_card.rb
@@ -6,16 +6,16 @@ class Ecstore::MemberCard < Ecstore::Base
 	belongs_to :buyer, :foreign_key=>"buyer_id",:class_name=>"User"
 	belongs_to :user, :foreign_key=>"user_id",:class_name=>"User"
 
-	validates :user_tel,
-			   :presence=>{:presence=>true,:message=>"请填写手机号码"},
-			   :if=>Proc.new{ |c| c.card.card_type == 'A' }
+	# validates :user_tel,
+	# 		   :presence=>{:presence=>true,:message=>"请填写手机号码"},
+	# 		   :if=>Proc.new{ |c| c.card.card_type == 'A' }
 
-	validates :user_tel,
-			   :format=>{:with=>/^\d{11}$/, :multiline => true,:message=>"使用者手机号码必须是11位数字"},
-			   :if=>Proc.new{ |c| c.card.card_type == 'A' }
+	# validates :user_tel,
+	# 		   :format=>{:with=>/^\d{11}$/, :multiline => true,:message=>"使用者手机号码必须是11位数字"},
+	# 		   :if=>Proc.new{ |c| c.card.card_type == 'A' }
 
-	validates :buyer_tel,:presence=>{:presence=>true,:message=>"请填写手机号码"}
-	validates :buyer_tel,:format=>{:with=>/^\d{11}$/, :multiline => true,:message=>"购买者手机号码必须是11位数字"}
+	# validates :buyer_tel,:presence=>{:presence=>true,:message=>"请填写手机号码"}
+	# validates :buyer_tel,:format=>{:with=>/^\d{11}$/, :multiline => true,:message=>"购买者手机号码必须是11位数字"}
 	
 	after_save :sell_card!
 
